a3129552088f559508ce3347c42b9c0df4e7e39f
Martin Martinez <investingmtz@gmail.com>
2021-01-01 12:34:01 -0600

    Mostrando letras random

--- corean.py
+++ corean.py
@@ -7,8 +7,14 @@ def letra_random():
         print(random.choice(letras_español))
 
 def main():
-    pass
+    otra_vez = ''
 
+    while otra_vez != 'si':
+        print('Te mostrare una letra en español y tu debes escribirla en coreano')
+        letra_random()
+
+        otra_vez = input('¿Quieres que te muestre otra letra? (si/no)')
+        
 
 if __name__ == "__main__":
     main()
