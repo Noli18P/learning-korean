40d62d7fac3d5bfc6907b26205797145b7c98636
Martin Martinez <investingmtz@gmail.com>
2021-01-01 12:32:07 -0600

    Script que me ayudará a aprender coreano

---
+++ corean.py
@@ -0,0 +1,14 @@
+import random
+
+def letra_random():
+    letras_español = ['m','n','r','i','a','o abierta','u estirada','u','o']
+
+    for i in letras_español:
+        print(random.choice(letras_español))
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
